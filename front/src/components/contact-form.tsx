1cb298f684d270e45227cb22d4911964a1690472
amine <jmalmohamedamine1@gmail.com>
2025-07-28 18:04:04 +0100

    changed the mail

--- front/src/components/contact-form.tsx
+++ front/src/components/contact-form.tsx
@@ -24,7 +24,7 @@ export function ContactForm() {
     try {
       // Using formsubmit.co - Note: This service has HTTPS security concerns
       // Alternative: You could use a server action or other form service
-      const response = await fetch("https://formsubmit.co/emm.sfax@gmail.com", {
+      const response = await fetch("https://formsubmit.co/emm.sfax.command.comerce@gmail.com", {
         method: "POST",
         body: formData,
       })
